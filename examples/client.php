d3e8d7b736a042678e5a27217db66d89c39a1447
Aztech <thibaud@aztech.io>
2014-08-22 16:35:22 +0200

    Add examples

--- examples/client.php
+++ examples/client.php
@@ -2,16 +2,31 @@
 
 include __DIR__ . '/../vendor/autoload.php';
 
-use Aztech\Zero\KeyValuePair;
-$context = new \ZMQContext();
-$publisher = new \ZMQSocket($context, \ZMQ::SOCKET_SUB);
-$publisher->connect('tcp://localhost:5557');
-$publisher->setSockOpt(\ZMQ::SOCKOPT_SUBSCRIBE, '');
+use Symfony\Component\Console\Logger\ConsoleLogger;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Aztech\Events\Plugins\ZeroMq\PubSubTransport;
+use Aztech\Events\Plugins\ZeroMq\SocketWrapper;
 
-$sequence = 0;
+$logger = new ConsoleLogger(new ConsoleOutput(ConsoleOutput::VERBOSITY_DEBUG));
+$context = new \ZMQContext();
+
+$subscriber = new \ZMQSocket($context, \ZMQ::SOCKET_SUB);
+$subscriber = new SocketWrapper($subscriber, 'tcp://127.0.0.1:5557', $logger);
+
+$publisher = new \ZMQSocket($context, \ZMQ::SOCKET_PUB);
+$publisher = new SocketWrapper($publisher, 'tcp://127.0.0.1:5557', $logger);
+
+$transport = new PubSubTransport($publisher, $subscriber, $logger);
 
 while (true) {
-    $kv = KeyValuePair::recv($publisher);
-    var_dump($kv);
+    $next = $transport->read();
+    
+    if (substr_count(strtolower($next), 'nanou') > 0) {
+        $logger->critical('<3 : ' . $next);
+    }
+    else {
+        $logger->info('Server said : ' . $next);
+    }
+    
     sleep(1);
 }
--- examples/server.php
+++ examples/server.php
@@ -2,9 +2,26 @@
 
 include __DIR__ . '/../vendor/autoload.php';
 
-use Aztech\Zero\KeyValuePair;
+use Symfony\Component\Console\Logger\ConsoleLogger;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Aztech\Events\Plugins\ZeroMq\PubSubTransport;
+use Aztech\Events\Plugins\ZeroMq\SocketWrapper;
+use Aztech\Events\Core\Event;
+
+$logger = new ConsoleLogger(new ConsoleOutput(ConsoleOutput::VERBOSITY_DEBUG));
 $context = new \ZMQContext();
+
+$subscriber = new \ZMQSocket($context, \ZMQ::SOCKET_SUB);
+$subscriber = new SocketWrapper($subscriber, 'tcp://127.0.0.1:5557', $logger);
+
 $publisher = new \ZMQSocket($context, \ZMQ::SOCKET_PUB);
-$publisher->bind('tcp://*:5557');
+$publisher = new SocketWrapper($publisher, 'tcp://127.0.0.1:5557', $logger);
 
+$transport = new PubSubTransport($publisher, $subscriber, $logger);
 
+$in = fopen('php://stdin', 'w');
+
+while ($line = fgets($in)) {
+    $transport->write(new Event('cookie.monster'), trim($line));
+    sleep(1);
+}
